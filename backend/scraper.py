ee10392c8a4d92e6db2ea3b6b358d4e121243cc4
Krish120003 <krish120003@gmail.com>
2024-05-18 15:38:08 -0400

    refactor: make a get_project_details function

--- backend/scraper.py
+++ backend/scraper.py
@@ -8,50 +8,67 @@ import requests
 baseURL = "https://deltahacks-x.devpost.com/"
 projectGal = baseURL + "project-gallery"
 
-page = requests.get(projectGal)
+# page = requests.get(projectGal)
+
 
 # print(page.text)
+def get_project_details(link):
+    """
+    This function scrapes project details from a given website.
 
-soup = BeautifulSoup(page.content, "html.parser")
+    Args:
+        link (str): The URL of the project webpage.
 
-# go to id = submission-gallery
+    Returns:
+        dict: A dictionary containing the extracted project details. Keys include:
+            title (str): Project title
+            description (str): Project description (short)
+            full_description (str): Full project description (might include multiple sections)
+            github_url (str): URL to the project's Github repository (if found)
+            team_members (list): List of URLs to team members' profiles (if found)
+    """
 
-# mainContainer = soup.find(id="container")
-# find main section
-# mainSection = mainContainer.find(id="main")
-# innerSection = mainContainer.find("section", class_=["large-9", "pull-3", "columns"])
-# innerSection2 = mainContainer.find("section", {"role": "main"})
-submissionGallery = soup.find(id="submission-gallery")
-
-# projs = soup.find_all("a", {"class": "block-wrapper-link fade link-to-software"})
-projects = soup.find_all("a", class_=["block-wrapper", "link.fade", "link-to-software"])
-
-# print(submissionGallery.prettify())
-projectLinks = []
-
-for link in projects:
-    print(link["href"])
-    projectLinks.append(link["href"])
-
-# find all pages
-# check if next page clickable
-nextPage = submissionGallery.find("a", rel=["next"])
-
-while nextPage is not None:
-    nextPage = baseURL + nextPage["href"]
-    print(nextPage)
-    # load next page
-    page = requests.get(nextPage)
+    page = requests.get(link)
     soup = BeautifulSoup(page.content, "html.parser")
-    submissionGallery = soup.find(id="submission-gallery")
-    # find all project links
-    projects = soup.find_all(
-        "a", class_=["block-wrapper", "link.fade", "link-to-software"]
-    )
-    for link in projects:
-        # print(link["href"])
-        projectLinks.append(link["href"])
-    nextPage = submissionGallery.find("a", rel=["next"])
 
-print(projectLinks)
-print(len(projectLinks))
+    project_details = {}
+
+    # Get title
+    title = soup.find(id="app-title")
+    if title:
+        project_details["title"] = title.text.strip()
+
+    # Get short description
+    description = soup.find("p", class_="large")
+    if description:
+        project_details["description"] = description.text.strip()
+
+    # Get full description (might include multiple sections)
+    fullInformation = soup.find(id="app-details-left")
+    if fullInformation:
+        innerDivs = fullInformation.find_all("div")
+        full_description = ""
+        for div in innerDivs:
+            full_description += div.get_text(" ").strip() + "\n"
+        project_details["full_description"] = full_description
+
+    # Get Github URL
+    softwareURLS = fullInformation.find(
+        "ul", attrs={"data-role": True}, class_="no-bullet"
+    )
+    if softwareURLS:
+        for url in softwareURLS.find_all("a"):
+            if "https://github.com" in url["href"]:
+                project_details["github_url"] = url["href"]
+                break  # Stop after finding the first Github URL
+
+    # Get team members
+    teamMembers = soup.find_all("li", class_="software-team-member")
+    members = []
+    for member in teamMembers:
+        memberLink = member.find("a", class_="user-profile-link")
+        if memberLink:
+            members.append(memberLink["href"])
+    project_details["team_members"] = members
+
+    return project_details
