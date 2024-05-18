630665ff748b2bd9614fe08298e48c8b9e5e7930
Shivermist <shivermist0104@gmail.com>
2024-05-18 11:53:33 -0400

    feat: extracted github repo links from users

--- backend/scraperBetter.py
+++ backend/scraperBetter.py
@@ -36,7 +36,7 @@ print(len(projectLinks))
 allTeams = []
 
 # go through each project found
-for link in projectLinks[0:3]:
+for link in projectLinks:
     page = requests.get(link)
     soup = BeautifulSoup(page.content, "html.parser")
 
@@ -82,3 +82,70 @@ for link in projectLinks[0:3]:
 
 print(allTeams)
 # go through all of their projects and do checks
+
+
+# # teams
+# hackerURL = "https://devpost.com/kennyzhao-code"
+
+def getProjectsFromHacker(hackerURL):
+    # checking first page
+    # desiredStartPage = "project-gallery"
+    page = requests.get(hackerURL)
+    # curPage = page
+    # projects = []
+    soup = BeautifulSoup(page.content, "html.parser")
+    projects = soup.find_all(
+        "a", class_=["block-wrapper-link", "fade", "link-to-software"]
+    )
+    # curPage = soup.find("a", rel=["next"])
+
+    # prints the projects in a list
+    projectLinks = []
+    for link in projects:
+        projectLinks.append(link["href"])
+    print(projectLinks)
+    print(len(projectLinks))
+    # allTeams = []
+
+    projectRepos = []
+
+    for link in projectLinks:
+        page = requests.get(link)
+        soup = BeautifulSoup(page.content, "html.parser")
+
+        # # get project title
+        # title = soup.find(id="app-title")
+        # print(title.text)
+
+        # # get project description
+        # description = soup.find("p", class_="large")
+        # print(description.text)
+
+        # get all project description
+        fullInformation = soup.find(id="app-details-left")
+        # ul tags is class=cp-tag
+        # if fullInformation is not None:
+        #     innerDivs = fullInformation.find_all("div")
+
+            # for div in innerDivs[1:]:
+            #     print(div.text)
+
+        # get github url (if it exists)
+        softwareURLS = fullInformation.find(
+            "ul", attrs={"data-role": True}, class_="no-bullet"
+        )
+        # find URLS containing github
+        if softwareURLS is not None:
+            for url in softwareURLS.find_all("a"):
+                if "https://github.com" in url["href"]:
+                    github = url["href"]
+                    projectRepos.append(github)
+
+        
+    print(projectRepos)
+
+for team in allTeams:
+    print(team)
+    for member in team:
+        print(member)
+        getProjectsFromHacker(member)
