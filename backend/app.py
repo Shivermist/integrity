7c3b29f620be00ac95b72d18c2bfe486b61ea040
Krish120003 <krish120003@gmail.com>
2024-05-19 03:45:55 -0400

    feat: very basic layout

--- backend/app.py
+++ backend/app.py
@@ -1,26 +1,44 @@
-from taipy.gui import Gui
+from taipy.gui import Gui, notify
 import taipy.gui.builder as tgb
-from math import cos, exp
+import re
+
+devposturl = ""
 
 
-value = 10
+def on_change(state, var_name, var_value):
+    print(f"Variable {var_name} changed to {var_value}")
+    return state
 
 
-def compute_data(decay: int) -> list:
-    return [cos(i / 6) * exp(-i * decay / 600) for i in range(100)]
+def on_submit_button_clicked(state):
+    print(state.devposturl, type(state.devposturl))
 
+    url = state.devposturl
+    print("URL is", url)
 
-def on_slider(state):
-    state.data = compute_data(state.value)
+    if not re.match(r"https://devpost.com/.*", url):
+        notify("Invalid URL", "Please enter a valid Devpost URL")
+        return state
+
+    return state
 
 
 with tgb.Page() as page:
-    tgb.text(value="# Taipy Getting Started", mode="md")
-    tgb.text(value="Value: {value}")
-    tgb.slider(value="{value}", on_change=on_slider)
-    tgb.chart(data="{data}")
+    with tgb.layout(columns="1 2"):
+        with tgb.part():
+            tgb.text(value="# Taipy Getting Started", mode="md")
+            tgb.text(value="Devpost URL:")
+            with tgb.layout(columns="1 1", class_name="button_thing"):
+                tgb.input(
+                    "{devposturl}",
+                )
+                tgb.button("Investigate!", on_action=on_submit_button_clicked)
 
-data = compute_data(value)
+        with tgb.part():
+            # tgb.text("lkdsajflak {devposturl}")
+            pass
+
 
 if __name__ == "__main__":
-    Gui(page=page).run(title="Dynamic chart")
+    g = Gui(page)
+    g.run(title="Dynamic chart", debug=True, port=8000, use_reloader=True)
