436e7b6d755f0f5742bb0bd28a015fc5737ef4b4
Krish120003 <krish120003@gmail.com>
2024-05-19 05:16:23 -0400

    feat: use streamlit

--- backend/main.py
+++ backend/main.py
@@ -1,97 +1,71 @@
-# from typing import List
+from dataclasses import dataclass
+import re
+import streamlit as st
 
-# from fastapi import FastAPI
-# from sentence_transformers import SentenceTransformer
-# import numpy as np
-
-# app = FastAPI()
-
-# import scraper
-
-# # model = SentenceTransformer(
-# #     "sentence-transformers/all-mpnet-base-v2"
-# # )  # this one is shit
-
-# # model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2") # this one is better
-
-# model = SentenceTransformer("Alibaba-NLP/gte-large-en-v1.5", trust_remote_code=True)
+import scraper
 
 
-# def get_related(project_link, team_members):
-#     """
-#     returns all projects done by the team members of this project
-#     """
-
-#     links = []
-
-#     for member in team_members:
-#         links.extend(scraper.get_hacker_projects(member))
-
-#     links = set(links)
-#     links.remove(project_link)
-#     return list(links)
+st.image("./integrity_logo.svg")
+st.write("### Find suspicious projects on your hackathon!")
 
 
-# def compare(src, targets):
-#     embeddings = model.encode([src, *targets])
+form = st.form(key="url form")
+url = form.text_input("Devpost URL:")
+form.form_submit_button("Analyze")
 
-#     a = embeddings[0]
-#     b = embeddings[1:]
+st.divider()
 
-#     cos_sim = np.dot(a, b.T) / (np.linalg.norm(a) * np.linalg.norm(b))
+regex = r".*.devpost\.com/$"
+if url and not (re.match(regex, url)):
+    st.error("Invalid URL, please enter a valid Devpost URL")
 
-#     return cos_sim.tolist()
+elif url:
+    with st.spinner("Getting Hackathon Details..."):
+        project_links, header = scraper.get_project_gallery(url)
 
+    progress_text = "Operation in progress. Please wait."
+    my_bar = st.progress(0, text=progress_text)
 
-# @app.get("/analyze")
-# def analyze(devpost_url: str):
-#     # Step 1:
-#     # Fetch the data from the devpost_url
+    st.image(header)
+    st.write("## Projects")
 
-#     project_links = scraper.get_project_gallery(devpost_url)
+    for i, link in enumerate(project_links[:5]):
+        with st.spinner(f"Fetching project {link}..."):
+            project_details = scraper.get_project_details(link)
 
-#     # Step 2:
-#     # Fetch the data from the project links
+        st.write(f"### [{project_details['title']}]({link})")
+        st.write(project_details["description"])
 
-#     details = {}
+        my_bar.progress((i + 1) / len(project_links))
 
-#     for project in project_links:
+        with st.spinner("Comparing to related projects..."):
+            links_to_fetch = scraper.get_related(link, project_details["team_members"])
 
-#         # if we have proj
+            compare_bar = st.progress(0, text="Comparing projects...")
+            content_to_compare = []
+            for i, link in enumerate(links_to_fetch):
+                fetched = scraper.get_project_details(link)
 
-#         project_details = scraper.get_project_details(project)
-#         details[project] = project_details
+                to_compare = fetched["description"] + fetched["full_description"]
+                if "How we built it" in to_compare:
+                    idx = to_compare.find("How we built it")
+                    to_compare = to_compare[:idx]
+                content_to_compare.append(to_compare)
 
-#         links_to_fetch = get_related(project, project_details["team_members"])
+                compare_bar.progress((i + 1) / len(links_to_fetch) * 2)
 
-#         content_to_compare: List[str] = []
-#         for link in links_to_fetch:
-#             fetched = scraper.get_project_details(link)
+            source_description = (
+                project_details["description"] + project_details["full_description"]
+            )
+            if "How we built it" in source_description:
+                idx = source_description.find("How we built it")
+                source_description = source_description[:idx]
 
-#             to_compare = fetched["description"] + fetched["full_description"]
-#             if "How we built it" in to_compare:
-#                 idx = to_compare.find("How we built it")
-#                 to_compare = to_compare[:idx]
-#             content_to_compare.append(to_compare)
+            for i, content in enumerate(content_to_compare):
 
-#         # run comparison check
-
-#         source_description = (
-#             project_details["description"] + project_details["full_description"]
-#         )
-#         if "How we built it" in source_description:
-#             idx = source_description.find("How we built it")
-#             source_description = source_description[:idx]
-
-#         e = compare(
-#             source_description,
-#             content_to_compare,
-#         )
-#         print(project_details, content_to_compare)
-#         print(e)
-#         exit(1)
-
-#     # For each team member, gather details on all their previous projects
-
-#     # RETURN ANALYSIS?
-#     return details
+                comp = scraper.compare(
+                    source_description,
+                    content,
+                )
+        # st.write(project_details, content_to_compare)
+        # st.write(e)
